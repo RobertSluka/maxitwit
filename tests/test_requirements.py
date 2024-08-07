ae73cc86475a71cf2edda80580ab82e74152712b
r.sluka <robosluka11@gmail.com>
2024-08-07 16:17:45 +0200

    chore(vagrant): set up the vagrant file

---
+++ tests/test_requirements.py
@@ -0,0 +1,3 @@
+# when you run pip install -r requirements.txt, pip will install these packages at the specified versions. This ensures that your testing environment is consistent and reproducible, which helps prevent issues caused by differences in package versions.
+pytest==8.0.0,
+requests==2.25.1
